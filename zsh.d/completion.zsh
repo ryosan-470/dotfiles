499cbb1184fd7afea614e6ce707d91853edfe401
Ryosuke Sato <ryosuke.sato@mixi.co.jp>
2023-06-14 14:25:09 +0900

    check command existance

--- zsh.d/completion.zsh
+++ zsh.d/completion.zsh
@@ -1,8 +1,10 @@
 # 補完系のコマンドを追加する
 # Homebrew のデフォルトの ZSH のパスを読み込む
-homebrew_zsh_completion_path=$(brew --prefix)/share/zsh/site-functions
-if [ -e ${homebrew_zsh_completion_path} ]; then
-  fpath=($homebrew_zsh_completion_path $fpath)
+if [ ! command -v brew &> /dev/null ]; then
+  homebrew_zsh_completion_path=$(brew --prefix)/share/zsh/site-functions
+  if [ -e ${homebrew_zsh_completion_path} ]; then
+    fpath=($homebrew_zsh_completion_path $fpath)
+  fi
 fi
 
 autoload -Uz compinit && compinit
